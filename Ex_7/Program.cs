76aee64fe688a3e1b9147d3bc4ed8261ca64ee46
karolinaaa2 <hryhorchuk.karolina@chnu.edu.ua>
2024-10-05 00:39:34 +0300

    Add project files.

---
+++ Ex_7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Число парне.");
+        }
+        else
+        {
+            Console.WriteLine("Число непарне.");
+        }
+
+        if (number % 3 == 0)
+        {
+            Console.WriteLine("Число ділиться на 3.");
+        }
+        else
+        {
+            Console.WriteLine("Число не ділиться на 3.");
+        }
+    }
+}
+
